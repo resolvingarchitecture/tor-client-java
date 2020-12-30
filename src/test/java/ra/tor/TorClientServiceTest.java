c98eb506cea1895cd79960edb73a8f96a918de77
objectorange <brian@resolvingarchitecture.io>
2020-12-30 18:49:12 -0500

    additional test methods; commented out non-working URLs

--- src/test/java/ra/tor/TorClientServiceTest.java
+++ src/test/java/ra/tor/TorClientServiceTest.java
@@ -52,7 +52,7 @@ public class TorClientServiceTest {
 
     @Test
     @Order(2)
-    public void verifyClient() {
+    public void verifyClientWithOnion() {
         Envelope envelope = Envelope.documentFactory();
         try {
             // Secure Drop Onion Site
@@ -65,7 +65,7 @@ public class TorClientServiceTest {
         envelope.setHeader(Envelope.HEADER_CONTENT_TYPE, "text/html");
         envelope.setAction(Envelope.Action.GET);
         service.sendOut(envelope);
-        String html = new String((byte[]) DLC.getContent(envelope));
+        String html = new String((byte[]) envelope.getContent());
         LOG.info(html);
         String secureDropMessage = "Share and accept documents securely";
         if(!html.contains(secureDropMessage)) {
@@ -74,8 +74,54 @@ public class TorClientServiceTest {
         assertTrue(html.contains(secureDropMessage));
     }
 
+    @Test
+    @Order(3)
+    public void verifyClientWithHTTPS() {
+        Envelope envelope = Envelope.documentFactory();
+        try {
+            // Bitcoin Memmpool Site
+            envelope.setURL(new URL("https://mempool.space/api/v1/fees/recommended"));
+        } catch (MalformedURLException e) {
+            LOG.severe(e.getLocalizedMessage());
+            fail();
+            return;
+        }
+        envelope.setHeader(Envelope.HEADER_CONTENT_TYPE, Envelope.HEADER_CONTENT_TYPE_JSON);
+        envelope.setAction(Envelope.Action.GET);
+        service.sendOut(envelope);
+        String res = new String((byte[]) envelope.getContent());
+        LOG.info(res);
+        String msg = "fastestFee";
+        if(!res.contains("fastestFee")) {
+            LOG.warning("Verify Memfee site is accessible at: https://mempool.space/api/v1/fees/recommended and contains 'fastestFee' in json.");
+        }
+        assertTrue(res.contains(msg));
+    }
+
 //    @Test
-//    @Order(3)
+//    @Order(4)
+//    public void verifyClientWithOnionJSON() {
+//        Envelope envelope = Envelope.documentFactory();
+//        try {
+//            envelope.setURL(new URL("http://wizpriceje6q5tdrxkyiazsgu7irquiqjy2dptezqhrtu7l2qelqktid.onion"));
+//        } catch (MalformedURLException e) {
+//            LOG.severe(e.getLocalizedMessage());
+//            fail();
+//            return;
+//        }
+//        envelope.setHeader(Envelope.HEADER_CONTENT_TYPE, Envelope.HEADER_CONTENT_TYPE_JSON);
+//        envelope.setAction(Envelope.Action.GET);
+//        service.sendOut(envelope);
+//        String res = new String((byte[]) envelope.getContent());
+//        LOG.info(res);
+//        String msg = "fastestFee";
+//        if(!res.contains("fastestFee")) {
+//            LOG.warning("Verify Memfee site is accessible at: https://mempool.space/api/v1/fees/recommended and contains 'fastestFee' in json.");
+//        }
+//        assertTrue(res.contains(msg));
+//    }
+//    @Test
+//    @Order(5)
 //    public void verifyHiddenService() {
 //        Envelope envelope = Envelope.documentFactory();
 //        try {
